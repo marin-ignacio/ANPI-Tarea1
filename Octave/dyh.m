fbeea9640f008fac28882d4a42ddd0dba13ae33f
Maesly Velásquez Bone <nadieska23@outlook.es>
2019-08-25 14:22:53 -0600

    Add files via upload

---
+++ Octave/dyh.m
@@ -0,0 +1,29 @@
+
+function []= dyh(func,x,tol, graf =1)
+  itera = 0;
+  error = [];
+  iteracion = [];
+  tempTol = Inf;
+  while tempTol >= tol
+    itera += 1;
+    fx = func(x);
+    w = x + func(x);
+    fw = func(w);
+    d = x - func(x);
+    fd = func(d);
+    
+    z = x -(2*(fx^2))/(fw-fd);
+    fz = func(z);
+    
+    xAprox = x -((2*fx*(fz-fx))/(fw-fd));
+    x = xAprox;
+    tempTol = abs(func(x));
+    error(end+1)= tempTol;
+    iteracion(end+1) = itera; 
+  endwhile
+  if graf==1
+    plot(iteracion,error);
+    ylabel('Errores');
+    xlabel('Iteraciones');
+  endif
+endfunction
---
+++ Octave/ssm.m
@@ -0,0 +1,27 @@
+%Método de Homeier
+%
+function [x] = ssm(f,x,tol,graf = 1)
+  itera=0;
+  error=[];
+  iteracion=[];
+  tempTol=Inf;
+  while tempTol>= tol
+    itera=itera+1;
+    fx=f(x);
+    z=x+f(x);
+    fz=f(z);
+    y=(x-((f(x)^2)/(fz-fx)));
+    fy=f(y);   
+    xAprox= (x - (fx^3)/((fz-fx)*(fx-fy)));
+    x=xAprox;
+    tempTol = abs(f(x))
+    error(end + 1)= tempTol
+    iteracion(end + 1)=itera
+  endwhile
+  if graf==1
+    plot(iteracion,error);
+    ylabel('Errores');
+    xlabel('Iteraciones');
+  endif
+  disp(error);
+endfunction
