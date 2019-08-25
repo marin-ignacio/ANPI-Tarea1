f0246176febcde0e45ff3edf042fa9a894e971c6
Maesly Velásquez Bone <nadieska23@outlook.es>
2019-08-25 14:23:17 -0600

    Add files via upload

---
+++ Python/DYH.py
@@ -0,0 +1,77 @@
+from sympy import *
+from numpy import *
+from matplotlib.pyplot import *
+
+'''
+Función que retorna el valor evaluado en la función
+'''
+def evaluate(func,x):
+    return eval(func)
+'''
+Método de Dehghan and Hajarian
+Método basado en el método de Steffensen, reemplaza la primera aproximación
+con una aproximación central 
+Parametros:
+    | -----------
+    |   funct :
+    |       Texto que representa la función f(x) = 0
+    |   xo :
+    |       Valor inicial de las iteraciones
+    |   tol :
+    |       Criterio de parada
+    |   graf : 
+    |       Un número, si es 1 se muestra el gráfico 0 no se muestra
+Salidas:
+    | --------
+    |   x_aprox :
+    |       Aproximación de la solución a f(x) = 0
+    |   iter :
+    |       Número de iteraciones utilizado para realizar la aproximación
+    |   graf : 
+    |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
+
+Referencias:
+https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2F1-s2.0-S0096300311002748-main.pdf'''
+
+def DehghanANDHajarian(func, xo,tol,graf = 1):
+    try:
+        f = lambda x: eval(funct, {'x': x, 'pi': pi, 'e': e,
+                                  'exp': exp, 'log': log, 'sqrt': sqrt,
+                                  'cos': cos, 'sin': sin, 'tan': tan})
+        x = xo
+        itera = 0
+        error = []
+        iteracion = []
+        tempTol = Inf
+
+        while(tempTol >= tol):
+            try:
+                itera += 1
+
+                fx = evaluate(func,x)
+                w = x + eval(func)
+                fw = evaluate(func,w)
+                d = x - eval(func)
+                fd = evaluate(func,d)
+
+                z = x - ((2*(fx**2))/(fw - fd))
+                fz = evaluate(func,z)
+                
+                xAprox = x - ((2*fx*(fz-fx))/(fw -fd))
+
+                x = xAprox
+                tempTol = abs(eval(func))
+                error.append(tempTol)
+                iteracion.append(itera)
+            except ZeroDivisionError:
+                print('ERROR: división por cero')
+                break
+        if(graf == 1):
+            plot(iteracion,error)
+            ylabel('Errores')
+            xlabel('Iteraciones')
+            show()
+    except (NameError, SyntaxError):
+        
+        print('ERROR: La función ingresada tiene una sintaxis incorrecta!')
+        
---
+++ Python/Homeier_method.py
@@ -0,0 +1,115 @@
+
+from sympy import *
+from numpy import *
+from matplotlib.pyplot import *
+
+'''
+Función que evalua la derivada en un punto
+'''
+def derivadaEvaluada(func, z):
+    x = Symbol('x')
+    y = eval(func)
+    y_dx = y.diff(x)
+    f = lambdify(x, y_dx, 'numpy')
+    return f(z)
+'''
+Función que evalua en un punto dado
+'''
+def evaluate(func, x):
+    return eval(func)
+'''
+Función que retorna la derivada
+'''
+def derivada(func):
+    x = Symbol('x')
+    f = eval(func)
+    derivada = f.diff(x)
+    return str(derivada)
+
+#Método numérico iterativo utilizando derivada
+'''
+Método de Homeier
+
+Este método basado en el de Newton, consiste en utilizar
+la derivada de la función evaluada en un punto y determinado
+de la siguiente manera :
+y = x - (m/(m+1))*(fx/dfx)
+
+Parametros:
+    | -----------
+    |   funct :
+    |       Texto que representa la función f(x) = 0
+    |   m :
+    |       Multiplicidad de las raices
+    |   x :
+    |       Valor inicial de las iteraciones
+    |   tol :
+    |       Criterio de parada
+    |   graf : 
+    |       Un número, si es 1 se muestra el gráfico 0 no se muestra
+Salidas:
+    | --------
+    |   x_aprox :
+    |       Aproximación de la solución a f(x) = 0
+    |   iter :
+    |       Número de iteraciones utilizado para realizar la aproximación
+    |   graf : 
+    |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
+
+Referencias:
+https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2FArtículo3.pdf
+'''
+
+def homeier(func, m, xo, tol,graf = 1):
+    try:
+        f = lambda x: eval(funct, {'x': x, 'pi': pi, 'e': e,
+                                  'exp': exp, 'log': log, 'sqrt': sqrt,
+                                  'cos': cos, 'sin': sin, 'tan': tan})
+        x = xo
+        itera = 0
+        error = []
+        iteration = []
+        tempTol = Inf
+
+        
+        while(tempTol >= tol):
+            try:
+                itera += 1
+                fx = evaluate(func,x)
+                
+                y = x - (m/(m+1))*(eval(func)/derivadaEvaluada(func,x))
+                fy = evaluate(func,y)
+                xAprox = x - ((m**2)*((m/(m+1))**(m-1))*(fx/derivadaEvaluada(func,y)))+m*(m-1)*(fx/derivadaEvaluada(func,x))
+                x = xAprox
+                tempTol = abs(eval(func))
+                error.append(tempTol)
+                iteration.append(itera)
+            except ZeroDivisionError:
+                print('ERROR: division por cero')
+                break
+        if(1 == graf):    
+            plot(iteration,error)
+            ylabel('Errores')
+            xlabel('Iteraciones')
+            show()
+    except (NameError, SyntaxError):
+        print('ERROR: La función ingresada tiene una sintaxis incorrecta!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+        
+        
