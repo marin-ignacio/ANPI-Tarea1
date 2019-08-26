1776b59cb7e140887c671ffd024d62babbdecad0
Maesly Velásquez Bone <nadieska23@outlook.es>
2019-08-25 18:49:47 -0600

    Update fd.py

--- Python/SolNE/fd.py
+++ Python/SolNE/fd.py
@@ -91,7 +91,143 @@ def sne_fd_1(funct, x, tol, graf=1):
         
         print('ERROR: La función ingresada tiene una sintaxis incorrecta!')
 
+#-----------------------------------------------------------------------------------
+# Method 2: Método de Jain basado en Steffens-Secant Method
+#-----------------------------------------------------------------------------------
 
+def sne_fd_2(func,xo,tol,graf = 1):
+    '''
+    Método de Jain, Steffensen-secant method
+    Este método está basado en el método de Newton, no utiliza derivadas para
+    calcular el valor aproximado de la función f(x) = 0
+    Parametros:
+        | ------
+        | func:
+        |    Texto que representa la función f(x) = 0
+        | xo:
+        |    Valor inicial de las iteraciones
+        | tol:
+        |   Criterio de parada
+        | graf:
+        |   Un número, si es 1 se muestra el gráfico 0 no se muestra
+    Salidas:
+        | --------
+        |   x_aprox :
+        |       Aproximación de la solución a f(x) = 0
+        |   iter :
+        |       Número de iteraciones utilizado para realizar la aproximación
+        |   graf : 
+        |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
+    Referencias:
+    https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2FArtículo3.pdf
+    '''
+    try:
+        f = lambda x: eval(funct, {'x': x, 'pi': pi, 'e': e,
+                                  'exp': exp, 'log': log, 'sqrt': sqrt,
+                                  'cos': cos, 'sin': sin, 'tan': tan})
+        
+        x = xo
+        itera = 0
+        error = []
+        iteracion = []
+        tempTol = Inf
+
+        while(tempTol >= tol):
+            itera += 1
+
+            fx = evaluate(func,x)
+            z = x + eval(func)
+            fz = evaluate(func,z)
+            
+            y = x - ((fx**2)/(fz-fx))
+            fy = evaluate(func,y)
+            
+
+            xAprox = x - (fx**3)/((fz-fx)*(fx-fy))
+            
+            x = xAprox
+            tempTol = abs(eval(func))
+            error.append(tempTol)
+            iteracion.append(itera)     
+        if(graf == 1):
+            plot(iteracion,error)
+            ylabel('Errores')
+            xlabel('Iteraciones')
+            show()
+    except (NameError, SyntaxError):
+        print('ERROR: La función ingresada tiene una sintaxis incorrecta!')
+        
+#-----------------------------------------------------------------------------------
+# Method 3: Método de Dehghan y Hajarian 
+#-----------------------------------------------------------------------------------
+def sne_fd_3(func, xo,tol,graf = 1):
+    '''
+    Método de Dehghan and Hajarian
+    Método basado en el método de Steffensen, reemplaza la primera aproximación
+    con una aproximación central 
+    Parametros:
+        | -----------
+        |   funct :
+        |       Texto que representa la función f(x) = 0
+        |   xo :
+        |       Valor inicial de las iteraciones
+        |   tol :
+        |       Criterio de parada
+        |   graf : 
+        |       Un número, si es 1 se muestra el gráfico 0 no se muestra
+    Salidas:
+        | --------
+        |   x_aprox :
+        |       Aproximación de la solución a f(x) = 0
+        |   iter :
+        |       Número de iteraciones utilizado para realizar la aproximación
+        |   graf : 
+        |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
+    Referencias:
+    https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2F1-s2.0-S0096300311002748-main.pdf
+    '''
+
+    try:
+        f = lambda x: eval(funct, {'x': x, 'pi': pi, 'e': e,
+                                  'exp': exp, 'log': log, 'sqrt': sqrt,
+                                  'cos': cos, 'sin': sin, 'tan': tan})
+        x = xo
+        itera = 0
+        error = []
+        iteracion = []
+        tempTol = Inf
+
+        while(tempTol >= tol):
+            try:
+                itera += 1
+
+                fx = evaluate(func,x)
+                y = x + evaluate(func,x)
+                fy = evaluate(func,y)
+                d = x - evaluate(func,x)
+                fd = evaluate(func,d)
+
+                z = x - ((2*(fx**2))/(fy - fd))
+                fz = evaluate(func,z)
+                
+                xAprox = x - ((2*fx*(fz-fx))/(fy -fd))
+
+                x = xAprox
+                tempTol = abs(eval(func))
+                error.append(tempTol)
+                iteracion.append(itera)
+            except ZeroDivisionError:
+                print('ERROR: división por cero')
+                break
+        if(graf == 1):
+            plot(iteracion,error)
+            ylabel('Errores')
+            xlabel('Iteraciones')
+            show()
+    except (NameError, SyntaxError):
+        
+        print('ERROR: La función ingresada tiene una sintaxis incorrecta!')
+    
 #-----------------------------------------------------------------------------------
 # Method 4: Ostrowski Free Derivative Method
 #-----------------------------------------------------------------------------------
