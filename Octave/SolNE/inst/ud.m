8be5b5fd26d44f027acce1f45f70f7d10d27440a
Maesly Velásquez Bone <nadieska23@outlook.es>
2019-08-25 21:16:08 -0600

    Update ud.m

--- Octave/SolNE/inst/ud.m
+++ Octave/SolNE/inst/ud.m
@@ -165,13 +165,132 @@ function [xAprox, iter] = dong(f, m, xo, tol, graf=1)
   end_try_catch
 endfunction
 
+%-------------------------------------------------------------------------------
+% Homeier's method: método 3
+%-------------------------------------------------------------------------------
+%    |
+%    | Function that implements the Homeier's method to solve f(x) = 0
+%    |
+%    | ---------------------------------------------------------------------------------
+%    | Parameters:
+%    | -----------
+%    |   funct :
+%    |       Text that represents the function f(x)
+%    |   m :
+%    |       Multiplicity of function's roots
+%    |   x :
+%    |       Initial value of the iterative method
+%    |   tol :
+%    |       Stop criterion of the iterative method
+%    |   graf : 
+%    |       A number, 1 show the graph, 0 don't show the graph
+%    |        
+%    | Returns:
+%    | --------
+%    |   x_aprox :
+%    |       Approximation to the solution of the equation f(x) = 0
+%    |   iter :
+%    |       Number of iterations used to approximate the zero of the function
+%    |   graf : 
+%    |       Graph of iteration (k) vs errors (|f(xk)|) of the iterative method
+%    | ---------------------------------------------------------------------------------
+%    |
+%    | The syntax rules for the input function are as follows:
+%    |     a. Use 'x' as variable name
+%    |     b. To multiply, add and subtract use '*', '+' and '-' respectively
+%    |     c. To place and exponent use '^'
+%    |     d. The function names of math library can be used (e.g., sqrt(), exp(), etc)
+%    |
+%-------------------------------------------------------------------------------
 
+function [] = sne_ud_3(func, m, x, tol, graf = 1)
+  itera = 0;
+  try
+    do
+      itera = itera+1;
+      fx = evaluate(func,x);
+      dfx = derivate(func,x)
+      y = x -(m/(m+1))*((m/(m+1))^(m-1))*(fx/dfx);
+      dy = evaluate(y);
+      xAprox = x -((m^2)*((m/(m+1))^(m-1))*(fx/dfx)) + m*(m-1)*(fx/dfx);
+      x = xAprox;
+      tempTol = abs(eval(f));
+      error(itera) = {tempTol};
+    until (tempTol <= tol);
+    if(graf==1)
+      plot(cell2mat(error));
+      ylabel('Errores (|f(x)|)');
+      xlabel('Iteraciones (k)');
+      title('Gráfica comparativa: Método de Homeier');
+    endif
+  catch err
+    warning(err.identifier, err.message);
+  end_try_catch
+endfunction
  
 %-----------------------------------------------------------------------------------
-% Method 5: Halley's method
+% Method 4: Halley's method
 %-----------------------------------------------------------------------------------
 %    |
 %    | Este método fue desarrollado por el matemático Edmund Halley
+%    | Información más detallada puede ser encontrada en la página 370 del artículo "One-point Newton-type iterative methods: A unified point ofview", ver ecuación 12 con G(w) de acuerdo con la tabla 2.
+%    | ------------------------------------------------------------------------------
+%    | Parameters:
+%    | -----------
+%    |    f   :
+%    |        Tipo de dato String. Es la ecuación matemática a utilizar.
+%    |    x0  :
+%    |        Tipo de dato Integer. Número inicial para comenzar la iteración.
+%    |    tol :
+%    |        Tipo de dato Float. Número mayor a cero que brinda condición de parada para la iteración.
+%    |    graf:
+%    |        Tipo de dato Integer. Indica si se desea obtener el gráfico de interaciones versus errores o no. Para ello se introduce 1 si se desea obtenerlo ó 0 si no.
+%    |        
+%    | Returns:
+%    | --------
+%    |    xAprox :
+%    |        Tipo de dato Float. El valor de x que se aproxima a la solución de la ecuación no lineal.
+%    |    iter   :
+%    |        Tipo de dato Integer. Brinda las iteraciones requeridas para brindar la tolerancia establecida.
+%    | ------------------------------------------------------------------------------
+%    |
+%    | The syntax rules for the input function are as follows:
+%    |     a. Use 'x' as variable name. Insert the function as string.
+%    |     b. To multiply, add and subtract use '*', '+' and '-' respectively
+%    |     c. To place and exponent use '**'
+%    |     d. The function names of math library can be used (e.g., sqrt(), exp(), etc)
+%    |
+function [xAprox, iter] = sne_ud_4(f, xo, tol, graf = 1)
+  x = xo;
+  iter = 0;
+  try
+    do
+      iter++;
+      firstDerivate = derivate(f, x);
+      secondDerivate = derivate2(f, x);
+      sqrtFirstDerivate = firstDerivate ^ 2;
+      w = (eval(f) * secondDerivate) / sqrtFirstDerivate;
+      xAprox = x - (2 / (2 - w)) * (eval(f) / firstDerivate);
+      x = xAprox;
+      tempTol = abs(eval(f));
+      error(iter) = {tempTol};
+    until (tempTol <= tol);
+    if(graf)
+      plot(cell2mat(error));
+      ylabel('Errores (|f(x)|)');
+      xlabel('Iteraciones (k)');
+      title('Gráfica comparativa: Método Halley');
+    endif
+  catch err
+    warning(err.identifier, err.message);
+  end_try_catch 
+endfunction
+
+%-----------------------------------------------------------------------------------
+% Method 5: Chebyshev's method
+%-----------------------------------------------------------------------------------
+%    |
+%    | Este método fue desarrollado por el matemático Pafnuti Chebyshev
 %    | Información más detallada puede ser encontrada en la página 370 del artículo "One-point Newton-type iterative methods: A unified point ofview", ver ecuación 12 con G(w) de acuerdo con la tabla 2.
 %    | ------------------------------------------------------------------------------
 %    | Parameters:
@@ -209,64 +328,6 @@ function [xAprox, iter] = sne_ud_5(f, xo, tol, graf = 1)
       secondDerivate = derivate2(f, x);
       sqrtFirstDerivate = firstDerivate ^ 2;
       w = (eval(f) * secondDerivate) / sqrtFirstDerivate;
-      xAprox = x - (2 / (2 - w)) * (eval(f) / firstDerivate);
-      x = xAprox;
-      tempTol = abs(eval(f));
-      error(iter) = {tempTol};
-    until (tempTol <= tol);
-    if(graf)
-      plot(cell2mat(error));
-      ylabel('Errores (|f(x)|)');
-      xlabel('Iteraciones (k)');
-      title('Gráfica comparativa: Método Halley');
-    endif
-  catch err
-    warning(err.identifier, err.message);
-  end_try_catch 
-endfunction
-
-%-----------------------------------------------------------------------------------
-% Method 6: Chebyshev's method
-%-----------------------------------------------------------------------------------
-%    |
-%    | Este método fue desarrollado por el matemático Pafnuti Chebyshev
-%    | Información más detallada puede ser encontrada en la página 370 del artículo "One-point Newton-type iterative methods: A unified point ofview", ver ecuación 12 con G(w) de acuerdo con la tabla 2.
-%    | ------------------------------------------------------------------------------
-%    | Parameters:
-%    | -----------
-%    |    f   :
-%    |        Tipo de dato String. Es la ecuación matemática a utilizar.
-%    |    x0  :
-%    |        Tipo de dato Integer. Número inicial para comenzar la iteración.
-%    |    tol :
-%    |        Tipo de dato Float. Número mayor a cero que brinda condición de parada para la iteración.
-%    |    graf:
-%    |        Tipo de dato Integer. Indica si se desea obtener el gráfico de interaciones versus errores o no. Para ello se introduce 1 si se desea obtenerlo ó 0 si no.
-%    |        
-%    | Returns:
-%    | --------
-%    |    xAprox :
-%    |        Tipo de dato Float. El valor de x que se aproxima a la solución de la ecuación no lineal.
-%    |    iter   :
-%    |        Tipo de dato Integer. Brinda las iteraciones requeridas para brindar la tolerancia establecida.
-%    | ------------------------------------------------------------------------------
-%    |
-%    | The syntax rules for the input function are as follows:
-%    |     a. Use 'x' as variable name. Insert the function as string.
-%    |     b. To multiply, add and subtract use '*', '+' and '-' respectively
-%    |     c. To place and exponent use '**'
-%    |     d. The function names of math library can be used (e.g., sqrt(), exp(), etc)
-%    |
-function [xAprox, iter] = sne_ud_6(f, xo, tol, graf = 1)
-  x = xo;
-  iter = 0;
-  try
-    do
-      iter++;
-      firstDerivate = derivate(f, x);
-      secondDerivate = derivate2(f, x);
-      sqrtFirstDerivate = firstDerivate ^ 2;
-      w = (eval(f) * secondDerivate) / sqrtFirstDerivate;
       xAprox = x - (1 + w/2) * (eval(f) / firstDerivate);
       x = xAprox;
       tempTol = abs(eval(f));
@@ -284,7 +345,7 @@ function [xAprox, iter] = sne_ud_6(f, xo, tol, graf = 1)
 endfunction
 
 %-------------------------------------------------------------------------------
-% OSTROWSKI: método 5
+% OSTROWSKI: método 6
 %-------------------------------------------------------------------------------
 %    |
 %    | Function that implements the Ostrowski's method to solve f(x) = 0
@@ -314,7 +375,7 @@ endfunction
 %    |     d. The function names of math library can be used (e.g., sqrt(), exp(), etc)
 %    |
 %-------------------------------------------------------------------------------
-function [x_k, iterations] = sne_ud_5(func, x0, tol)
+function [x_k, iterations] = sne_ud_6(func, x0, tol)
   x = x0;
   iterations = 0;
   do
