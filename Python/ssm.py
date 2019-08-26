ee179d341163c3db65ad6e7f8f1822de93d2710d
Maesly Velásquez Bone <nadieska23@outlook.es>
2019-08-25 18:38:10 -0600

    Update ssm.py

--- Python/ssm.py
+++ Python/ssm.py
@@ -8,32 +8,33 @@ Funcion que retorna el valor evaluado en la función
 def evaluate(func,x):
     return eval(func)
 
-'''
-Método de Jain, Steffensen-secant method
-Este método está basado en el método de Newton, no utiliza derivadas para
-calcular el valor aproximado de la función f(x) = 0
-Parametros:
-    | ------
-    | func:
-    |    Texto que representa la función f(x) = 0
-    | xo:
-    |    Valor inicial de las iteraciones
-    | tol:
-    |   Criterio de parada
-    | graf:
-    |   Un número, si es 1 se muestra el gráfico 0 no se muestra
-Salidas:
-    | --------
-    |   x_aprox :
-    |       Aproximación de la solución a f(x) = 0
-    |   iter :
-    |       Número de iteraciones utilizado para realizar la aproximación
-    |   graf : 
-    |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
-Referencias:
-https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2FArtículo3.pdf
-'''
+
 def ssm(func,xo,tol,graf = 1):
+    '''
+    Método de Jain, Steffensen-secant method
+    Este método está basado en el método de Newton, no utiliza derivadas para
+    calcular el valor aproximado de la función f(x) = 0
+    Parametros:
+        | ------
+        | func:
+        |    Texto que representa la función f(x) = 0
+        | xo:
+        |    Valor inicial de las iteraciones
+        | tol:
+        |   Criterio de parada
+        | graf:
+        |   Un número, si es 1 se muestra el gráfico 0 no se muestra
+    Salidas:
+        | --------
+        |   x_aprox :
+        |       Aproximación de la solución a f(x) = 0
+        |   iter :
+        |       Número de iteraciones utilizado para realizar la aproximación
+        |   graf : 
+        |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
+    Referencias:
+    https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2FArtículo3.pdf
+    '''
     try:
         f = lambda x: eval(funct, {'x': x, 'pi': pi, 'e': e,
                                   'exp': exp, 'log': log, 'sqrt': sqrt,
