4abc96e50f27d29dc566431bd14b76d8dde550e6
Maesly Velásquez Bone <nadieska23@outlook.es>
2019-08-25 18:36:23 -0600

    Update Homeier_method.py

--- Python/Homeier_method.py
+++ Python/Homeier_method.py
@@ -3,64 +3,68 @@ from sympy import *
 from numpy import *
 from matplotlib.pyplot import *
 
-'''
-Función que evalua la derivada en un punto
-'''
+
 def derivadaEvaluada(func, z):
+    '''
+    Función que evalua la derivada en un punto
+    '''
     x = Symbol('x')
     y = eval(func)
     y_dx = y.diff(x)
     f = lambdify(x, y_dx, 'numpy')
     return f(z)
-'''
-Función que evalua en un punto dado
-'''
+
 def evaluate(func, x):
+    '''
+    Función que evalua en un punto dado
+    '''
     return eval(func)
-'''
-Función que retorna la derivada
-'''
+
 def derivada(func):
+    '''
+    Función que retorna la derivada
+    '''
     x = Symbol('x')
     f = eval(func)
     derivada = f.diff(x)
     return str(derivada)
 
 #Método numérico iterativo utilizando derivada
-'''
-Método de Homeier
-
-Este método basado en el de Newton, consiste en utilizar
-la derivada de la función evaluada en un punto y determinado
-de la siguiente manera :
-y = x - (m/(m+1))*(fx/dfx)
-
-Parametros:
-    | -----------
-    |   funct :
-    |       Texto que representa la función f(x) = 0
-    |   m :
-    |       Multiplicidad de las raices
-    |   x :
-    |       Valor inicial de las iteraciones
-    |   tol :
-    |       Criterio de parada
-    |   graf : 
-    |       Un número, si es 1 se muestra el gráfico 0 no se muestra
-Salidas:
-    | --------
-    |   x_aprox :
-    |       Aproximación de la solución a f(x) = 0
-    |   iter :
-    |       Número de iteraciones utilizado para realizar la aproximación
-    |   graf : 
-    |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
-
-Referencias:
-https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2FArtículo3.pdf
-'''
 
 def homeier(func, m, xo, tol,graf = 1):
+    '''
+    Método de Homeier
+
+    Este método basado en el de Newton, consiste en utilizar
+    la derivada de la función evaluada en un punto y determinado
+    de la siguiente manera :
+    y = x - (m/(m+1))*(fx/dfx)
+
+    Parametros:
+        | -----------
+        |   funct :
+        |       Texto que representa la función f(x) = 0
+        |   m :
+        |       Multiplicidad de las raices
+        |   x :
+        |       Valor inicial de las iteraciones
+        |   tol :
+        |       Criterio de parada
+        |   graf : 
+        |       Un número, si es 1 se muestra el gráfico 0 no se muestra
+    Salidas:
+        | --------
+        |   x_aprox :
+        |       Aproximación de la solución a f(x) = 0
+        |   iter :
+        |       Número de iteraciones utilizado para realizar la aproximación
+        |   graf : 
+        |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
+
+    Referencias:
+    https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2FArtículo3.pdf
+    '''
+
     try:
         f = lambda x: eval(funct, {'x': x, 'pi': pi, 'e': e,
                                   'exp': exp, 'log': log, 'sqrt': sqrt,
