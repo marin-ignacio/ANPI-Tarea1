357c6003446eb0a6cbcea01601a8dd3164c8bebe
Maesly Velásquez Bone <nadieska23@outlook.es>
2019-08-25 20:06:36 -0600

    Update test.py

--- Python/MyApp/test.py
+++ Python/MyApp/test.py
@@ -1,4 +1,4 @@
-from solne import sne_ud_1, sne_ud_2, sne_fd_1
+from solne import sne_ud_1, sne_ud_2,sne_ud_3,sne_ud_4,sne_ud_5,sne_ud_6, sne_fd_1, sne_fd_2, sne_fd_3,sne_fd_4,sne_fd_5,sne_fd_6
 
 f = ''
 x0 = 0
