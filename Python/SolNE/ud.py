29fb28b4061a55aa7a9580870284768318266032
reiracm <reiracm@gmail.com>
2019-08-25 18:34:17 -0600

    Update ud.py

--- Python/SolNE/ud.py
+++ Python/SolNE/ud.py
@@ -192,6 +192,52 @@ def sne_ud_2(funct, m, x, tol, graf=1):
 #-----------------------------------------------------------------------------------
 
 
+def ostrowskis_method(fu,x0,tol):
+    '''
+    |
+    | Function that implements the Ostrowski's to solve f(x) = 0
+    |
+    | ------------------------------------------------------------------------------
+    | Parameters:
+    | -----------
+    |   fu :
+    |        Text that represents the function f(x)
+    |    x0 :
+    |        Initial value of the iterative method
+    |    tol :
+    |        Stop criterion of the iterative method
+    |        
+    | Returns:
+    | --------
+    |    x_k :
+    |       Approximation to the solution of the equation f(x) = 0
+    |    iterat :
+    |        Number of iterations used to approximate the zero of the function
+    | ------------------------------------------------------------------------------
+    |
+    | The syntax rules for the input function are as follows:
+    |     a. Use 'x' as variable name. Insert the function as string.
+    |     b. To multiply, add and subtract use '*', '+' and '-' respectively
+    |     c. To place and exponent use '**'
+    |     d. The function names of math library can be used (e.g., sqrt(), exp(), etc)
+    |
+    '''
+    f = lambda x: eval(fu, {'x': x, 'pi': pi, 'e': e,'exp': exp,
+                            'log': log, 'sqrt': sqrt,'cos': cos,
+                            'sin': sin, 'tan': tan})
+    iterat = 0                                           # Iterations counter                      
+    x = sp.Symbol('x')
+    dF = lambda x: diff(f,x)
+    
+
+    while abs(f(x0)) > tol:                              # Ending condition
+
+        y = (x0 - (f(x0) / (dF(x0))))                    # Ostrowski Formula
+        xk = y - (((f(x0) + 2 * f(y)) / (f(x0))) * (f(y) / dF(x0)))
+        iterat += 1
+        x0 = xk
+
+    return xk, iterat
 
 
 
