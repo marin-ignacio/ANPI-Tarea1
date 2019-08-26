35fc4cd4a0178a276635105d11d4a8a6fe861e1d
Maesly Velásquez Bone <nadieska23@outlook.es>
2019-08-25 18:23:14 -0600

    Add files via upload

---
+++ Octave/homeier_method.m
@@ -0,0 +1,40 @@
+pkg load symbolic %Importar symbolic
+format long
+
+function [r] = derivate(func, e)
+  f = @(x) eval(func);
+  syms x;
+  ff = f(x);
+  ffd = diff(ff, x);
+  df = function_handle(ffd);
+  r = df(e);
+endfunction
+
+function [r] = evaluate(f, x)
+  r = eval(f);
+endfunction
+  
+function [] = homeier_method(func, m, x, tol, graf = 1)
+  itera = 0;
+  try
+    do
+      itera = itera+1;
+      fx = evaluate(func,x);
+      dfx = derivate(func,x)
+      y = x -(m/(m+1))*((m/(m+1))^(m-1))*(fx/dfx);
+      dy = evaluate(y);
+      xAprox = x -((m^2)*((m/(m+1))^(m-1))*(fx/dfx)) + m*(m-1)*(fx/dfx);
+      x = xAprox;
+      tempTol = abs(eval(f));
+      error(itera) = {tempTol};
+    until (tempTol <= tol);
+    if(graf==1)
+      plot(cell2mat(error));
+      ylabel('Errores (|f(x)|)');
+      xlabel('Iteraciones (k)');
+      title('Gráfica comparativa: Método de Homeier');
+    endif
+  catch err
+    %warning(err.identifier, err.message);
+  end_try_catch
+endfunction
