0919d42b56062b2b2676af2b5fee68a7c04b5924
Maesly Velásquez Bone <nadieska23@outlook.es>
2019-08-25 18:37:24 -0600

    Update DYH.py

--- Python/DYH.py
+++ Python/DYH.py
@@ -7,33 +7,35 @@ Función que retorna el valor evaluado en la función
 '''
 def evaluate(func,x):
     return eval(func)
-'''
-Método de Dehghan and Hajarian
-Método basado en el método de Steffensen, reemplaza la primera aproximación
-con una aproximación central 
-Parametros:
-    | -----------
-    |   funct :
-    |       Texto que representa la función f(x) = 0
-    |   xo :
-    |       Valor inicial de las iteraciones
-    |   tol :
-    |       Criterio de parada
-    |   graf : 
-    |       Un número, si es 1 se muestra el gráfico 0 no se muestra
-Salidas:
-    | --------
-    |   x_aprox :
-    |       Aproximación de la solución a f(x) = 0
-    |   iter :
-    |       Número de iteraciones utilizado para realizar la aproximación
-    |   graf : 
-    |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
-
-Referencias:
-https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2F1-s2.0-S0096300311002748-main.pdf'''
 
 def DehghanANDHajarian(func, xo,tol,graf = 1):
+    '''
+    Método de Dehghan and Hajarian
+    Método basado en el método de Steffensen, reemplaza la primera aproximación
+    con una aproximación central 
+    Parametros:
+        | -----------
+        |   funct :
+        |       Texto que representa la función f(x) = 0
+        |   xo :
+        |       Valor inicial de las iteraciones
+        |   tol :
+        |       Criterio de parada
+        |   graf : 
+        |       Un número, si es 1 se muestra el gráfico 0 no se muestra
+    Salidas:
+        | --------
+        |   x_aprox :
+        |       Aproximación de la solución a f(x) = 0
+        |   iter :
+        |       Número de iteraciones utilizado para realizar la aproximación
+        |   graf : 
+        |       Gráfico de Iteraciones(k) Vs Errores(|f(x)|)
+
+    Referencias:
+    https://tecdigital.tec.ac.cr/dotlrn/classes/IDC/CE3102/S-2-2019.CA.CE3102.1/file-storage/view/Tareas%2Ftarea-1%2Fart-culos-cient-ficos%2F1-s2.0-S0096300311002748-main.pdf
+    '''
+
     try:
         f = lambda x: eval(funct, {'x': x, 'pi': pi, 'e': e,
                                   'exp': exp, 'log': log, 'sqrt': sqrt,
